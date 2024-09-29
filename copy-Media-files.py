dfc0df7d482632bc3ee364cfba6b95923ccfdf5f
Luis Pedro Coelho <luis@luispedro.org>
2024-09-29 17:14:36 +1000

    BUG Fix Media file identification
    URLs no longer need to start with "/Media/" ("Media/" is enough)

--- copy-Media-files.py
+++ copy-Media-files.py
@@ -4,8 +4,8 @@ from os import makedirs, path
 from sys import argv
 from glob import glob
 
-pat = re.compile(f'\/Media\/(?P<mid>.*)(?P<name>.*)\.(?P<ext>[^\'"]*)')
-pat_stepped = re.compile(f'\/Media\/(?P<mid>.*)(?P<name>.*)_stepped-')
+pat = re.compile(f'\/?Media\/(?P<mid>.*)(?P<name>.*)\.(?P<ext>[^\'"]*)')
+pat_stepped = re.compile(f'\/?Media\/(?P<mid>.*)(?P<name>.*)_stepped-')
 pat_fromString = re.compile(f"' \+ \(\$elm\$core\$String\$fromInt\(.+\) \+ '")
 
 if len(argv) >= 2:
@@ -19,7 +19,9 @@ with open(f'{target}/index.html', 'r') as ifile:
     for line in ifile:
         if m := pat.search(line):
             f = m.group(0)
-            # f includes the leading slash
+            # normalize f to always include the leading slash
+            if f.startswith('Media/'):
+                f = '/' + f
             makedirs(f'{target}{f[:f.rfind("/")]}', exist_ok=True)
             if path.exists(f[1:]):
                 shutil.copyfile(f[1:], f'{target}{f}')
@@ -30,6 +32,9 @@ with open(f'{target}/index.html', 'r') as ifile:
                 raise IOError('File not found: ' + f)
         elif m := pat_stepped.search(line):
             f = m.group(0)
+            # normalize f to always include the leading slash
+            if f.startswith('Media/'):
+                f = '/' + f
             makedirs(f'{target}{f[:f.rfind("/")]}', exist_ok=True)
             for f in glob(f'{f[1:]}*'):
                 shutil.copyfile(f, f'{target}/{f}')
